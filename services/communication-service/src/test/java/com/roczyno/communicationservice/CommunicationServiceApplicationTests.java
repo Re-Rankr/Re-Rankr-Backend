82afda728aa36b7ec89a9d23a3733996bc361284
roczyno <adiabaroczyno9@gmail.com>
2024-08-14 09:03:00 +0000

    add: project setup

---
+++ services/communication-service/src/test/java/com/roczyno/communicationservice/CommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roczyno.communicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
